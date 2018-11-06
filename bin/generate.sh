339eccdd31f911794ca5937c84915202f725b8b2
Daniel Baur <daniel.baur@uni-ulm.de>
2018-11-06 09:59:41 +0100

    Initial gh-pages branch

--- bin/generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar bin/swagger-codegen-cli.jar generate -i https://raw.githubusercontent.com/cloudiator/rest-swagger/master/swagger.yaml -l java --artifact-id java-rest --group-id io.github.cloudiator.client --artifact-version 0.3.0-SNAPSHOT --api-package io.github.cloudiator.rest.api --model-package io.github.cloudiator.rest.model --additional-properties serializableModel=true -DhideGenerationTimestamp=true
